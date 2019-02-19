bd80e668fda87c3cef12e6ab44ba1712349fc125
ZhangWei <350095093@qq.com>
2019-02-19 15:44:39 +0800

    add ping

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
-var checkToken = require('./router/checkToken');
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-const order = require("./router/order");
-const partners = require("./router/partners");
-const product = require("./router/product");
-const weixin = require("./router/weixin");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const logistic = require("./router/logistic");
+const qrcode = require("./router/qrcode");
+const login = require("./router/login");
 
-const authentication = require('./router/authentication');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
+app.use('/',(req,res)=>{
+    res.send({success:true,message:"ping successfully"})
+})
 app.use('/logistic',logistic);
 app.use('/qrcode',qrcode);
 app.use('/login',login);
+app.use('/',login);
 
-
-app.listen(3010, () => console.log('Now node.js instance is running, listening on port 3010!'))
+app.listen(3011, () => console.log('微信后端API服务器已启动，端口3011请运维人员做好准备!'))
